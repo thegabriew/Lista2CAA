b66b6a6cdfefde1811e2e3fc10b3488924017699
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-12-05 20:35:26 -0300

    Create Q3_MelhorCaso.py

---
+++ Q3_MelhorCaso.py
@@ -0,0 +1,18 @@
+def dois_soma(números: List[int], alvo: int) -> List[int]:
+
+  # Cria um dicionário para armazenar os números já vistos.
+  vistos = {}
+
+  # Percorre o vetor `números`.
+  for índice, número in enumerate(números):
+    # Verifica se o número `alvo - número` já foi visto.
+    alvo_complemento = alvo - número
+    if alvo_complemento in vistos:
+      # Retorna os índices dos dois números.
+      return [vistos[alvo_complemento], índice]
+    else:
+      # Armazena o número `número` no dicionário.
+      vistos[número] = índice
+
+  # Se chegar aqui, significa que não encontrou uma solução.
+  return None
