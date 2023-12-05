0632767dd327c9ca2d7849cd48f70a44d3cc6e56
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-12-05 20:30:14 -0300

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,25 +1,25 @@
-def three_way_partition(S: List[int], piv: int, p: int,
-                        r: int) -> Tuple[int, int]:
+def particao_de_tres_vias(S: List[int], pivo: int, p: int,
+                          r: int) -> Tupla[int, int]:
 
-  # Inicializa os ponteiros esquerdo e direito.
-  i = p
-  j = r
+    # Inicializa os ponteiros esquerdo e direito.
+    indice_esquerdo = p
+    indice_direito = r
 
-  # Percorre o array até que os ponteiros se encontrem.
-  while i <= j:
-    # Enquanto o elemento atual for menor que o pivô, mova-o para a esquerda.
-    while S[i] < piv:
-      i += 1
+    # Percorre o array até que os ponteiros se encontrem.
+    while indice_esquerdo <= indice_direito:
+        # Enquanto o elemento atual for menor que o pivô, mova-o para a esquerda.
+        while S[indice_esquerdo] < pivo:
+            indice_esquerdo += 1
 
-    # Enquanto o elemento atual for maior que o pivô, mova-o para a direita.
-    while S[j] > piv:
-      j -= 1
+        # Enquanto o elemento atual for maior que o pivô, mova-o para a direita.
+        while S[indice_direito] > pivo:
+            indice_direito -= 1
 
-    # Se os ponteiros ainda não se encontraram, troque os dois elementos.
-    if i <= j:
-      S[i], S[j] = S[j], S[i]
-      i += 1
-      j -= 1
+        # Se os ponteiros ainda não se encontraram, troque os dois elementos.
+        if indice_esquerdo <= indice_direito:
+            S[indice_esquerdo], S[indice_direito] = S[indice_direito], S[indice_esquerdo]
+            indice_esquerdo += 1
+            indice_direito -= 1
 
-  # Retorne os índices `q1` e `q2`.
-  return i, j
+    # Retorne os índices `q1` e `q2`.
+    return indice_esquerdo, indice_direito
